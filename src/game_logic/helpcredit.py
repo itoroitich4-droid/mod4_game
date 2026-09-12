b00d7873162255e249a37a6d839f93630832d4f8
itoroitich4-droid <itoroitich4@gmail.com>
2026-09-13 00:04:20 +0300

    epmty repo

--- src/game_logic/helpcredit.py
+++
@@ -1,6 +0,0 @@
-from utils.db1loader import load_data
-
-hlcrdata=load_data()
-
-help_info = hlcrdata["help"]
-credits = hlcrdata["credits"]
--- utils/db1loader.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def load_data():
-    with open("database/db1.json", "r") as file:
-        return json.load(file)
-
-
-#for  our games help and credit section read
